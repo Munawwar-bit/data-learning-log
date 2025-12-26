653ddc220d0e3b47c5a7ce63b4d18814a6936c12
Munawwar-bit <58093674+Munawwar-bit@users.noreply.github.com>
2025-12-26 14:47:32 +0530

    Select, where, order by
    Solved Exercises

--- sql/day1.sql
+++ sql/day1.sql
@@ -5,3 +5,100 @@ use sql_store;
 select * from customers
 where customer_id = 1
 order by first_name;
+
+## Practice Exercise #1:
+Based on the employees table below, select all fields from the employees table whose salary is less than or equal to $52,500 (no sorting is required):
+
+CREATE TABLE employees
+( employee_number int NOT NULL,
+  last_name char(50) NOT NULL,
+  first_name char(50) NOT NULL,
+  salary int,
+  dept_id int,
+  CONSTRAINT employees_pk PRIMARY KEY (employee_number)
+);
+
+INSERT INTO employees
+(employee_number, last_name, first_name, salary, dept_id)
+VALUES
+(1001, 'Smith', 'John', 62000, 500);
+
+INSERT INTO employees
+(employee_number, last_name, first_name, salary, dept_id)
+VALUES
+(1002, 'Anderson', 'Jane', 57500, 500);
+
+INSERT INTO employees
+(employee_number, last_name, first_name, salary, dept_id)
+VALUES
+(1003, 'Everest', 'Brad', 71000, 501);
+
+INSERT INTO employees
+(employee_number, last_name, first_name, salary, dept_id)
+VALUES
+(1004, 'Horvath', 'Jack', 42000, 501);
+
+### Answer:
+select * from employees
+where salary <= 52500;
+
+## Practice Exercise #2:
+Based on the suppliers table below, select the unique city values that reside in the state of California and order the results in descending order by city:
+
+CREATE TABLE suppliers
+( supplier_id int NOT NULL,
+  supplier_name char(50) NOT NULL,
+  city char(50),
+  state char(25),
+  CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id)
+);
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(100, 'Microsoft', 'Redmond', 'Washington');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(200, 'Google', 'Mountain View', 'California');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(300, 'Oracle', 'Redwood City', 'California');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(400, 'Kimberly-Clark', 'Irving', 'Texas');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(500, 'Tyson Foods', 'Springdale', 'Arkansas');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(600, 'SC Johnson', 'Racine', 'Wisconsin');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(700, 'Dole Food Company', 'Westlake Village', 'California');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(800, 'Flowers Foods', 'Thomasville', 'Georgia');
+
+INSERT INTO suppliers
+(supplier_id, supplier_name, city, state)
+VALUES
+(900, 'Electronic Arts', 'Redwood City', 'California');
+
+### Answer:
+select distinct city from suppliers
+where state = "California"
+order by city desc;
